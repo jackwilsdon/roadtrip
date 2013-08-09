4d18e99c93dba59e7abca2f480b2fcef3f1220e2
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-09 20:19:11 +0100

    Add trip list for user and start show method

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,2 +1,12 @@
 class TripController < ApplicationController
+  def list
+    unless @user
+      redirect_to :controller => "user", :action => "login"  
+    end
+    @trips = @user.trips
+  end 
+
+  def show
+    
+  end
 end
