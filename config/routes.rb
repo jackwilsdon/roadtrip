c425e12bf5c9f23e1a66df692ce4e75990819f97
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-09 02:53:04 +0100

    Add user and trip routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Roadtrip::Application.routes.draw do
   get "/", to: "stream#index"
+
   get "/user/login", to: "user#login"
   post "/user/login", to: "user#login_attempt"
+
+  get "/user/logout", to: "user#logout"
+
+  get "/user/:id", to: "user#show"
+
+  get "/trip/list", to: "trip#list"
+  get "/trip/:id", to: "trip#show"
   resources :user, :trip, :stream
 end
