e99730e57c6277a1ddea1a60873462ab208760e3
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-09 02:50:00 +0100

    Add index for trips accessible to the signed in user

--- app/controllers/stream_controller.rb
+++ app/controllers/stream_controller.rb
@@ -1,4 +1,19 @@
 class StreamController < ApplicationController
   def index
+    unless @user
+      redirect_to controller: "user", action: "login"
+      return
+    end
+
+    @trips = []
+    Trip.all.each do |trip|
+      if @user.can_access trip.id
+        @trips.push trip
+      end
+    end
+
+    if @trips.length > 1
+    	@trips = @trips.sort_by { |a, b| a.updated_at <=> b.updated_at }
+  	end
   end
 end
