c41b729423a49380cd571aa8c447734c9f22d794
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-09 02:47:06 +0100

    Add :trip_id to attr_accessible

--- app/models/waypoint.rb
+++ app/models/waypoint.rb
@@ -1,5 +1,5 @@
 class Waypoint < ActiveRecord::Base
-  attr_accessible :latitude, :longitude, :name
+  attr_accessible :latitude, :longitude, :name, :trip_id
   validates :latitude, :presence => true
   validates :longitude, :presence => true
   validates :name, :presence => true
