91e6b3f4209c089c9319140f62d7fa04966e3eb1
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-10 01:06:41 +0100

    Add auth method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   before_filter :before
+  helper_method :auth
 
   def before
     unless User.where(:id => session[:uid]).blank?
       @user = User.find(session[:uid])
     else
       @user = false
+    end
+  end
+
+  def auth
+    unless @user
+      redirect_to :controller => "user", :action => "login"
     end
   end
 end
