f9b1c56a3946d1cb8dd91a33c27b688912f6e093
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-10 09:13:22 +0100

    Add auth and trip creation

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -14,13 +14,39 @@ class TripController < ApplicationController
   end
 
   def new
+    auth
   end
 
   def new_submit
+    auth
+
     if params[:new_submit][:name].empty?
       flash[:notice] = "You need to provide a name!"
       flash[:class] = "alert-error"
       redirect_to :action => "new"
+    end
+
+    @trip = Trip.new(:name => params[:new_submit][:name], :user_id => @user.id)
+
+    unless @trip.valid?
+      flash[:notice] = "Validation error!"
+      flash[:class] = "alert-error"
+      redirect_to :action => "new"
+    end
+
+	@trip.save
+
+    flash[:notice] = "Created trip #{@trip.name}"
+    flash[:class] = "alert-success"
+    redirect_to :action => "edit"
+  end
+
+  def edit
+    auth
+    if Trip.where(:id => params[:id]).blank?
+      @trip = Trip.find(params[:id])
+    else
+      @trip = false
     end
   end
 end
