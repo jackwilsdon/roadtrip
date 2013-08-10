b02235cd28c0ce2470b708036bffe36ae7c05026
Jack Wilsdon <jack.wilsdon@gmail.com>
2013-08-10 02:30:31 +0100

    Add post for methods

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Roadtrip::Application.routes.draw do
   get "/user/:id", to: "user#show"
 
   get "/trip/list", to: "trip#list"
+  get "/trip/new", to: "trip#new"
+  post "/trip/new", to: "trip#new_submit"
+  get "/trip/edit", to: "trip#edit"
   get "/trip/:id", to: "trip#show"
   resources :user, :trip
 end
